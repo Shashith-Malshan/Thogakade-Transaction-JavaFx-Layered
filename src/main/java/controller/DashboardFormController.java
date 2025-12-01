46c69ae5ac571baf1736bdfc520a2105e4d42afc
Shashith-Malshan <malshanshashith318@gmail.com>
2025-12-01 14:39:39 +0530

    Add Dashboard UI and Dashboard Form Controller

---
+++ src/main/java/controller/DashboardFormController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DashboardFormController {
+
+    @FXML
+    private Button btnCustomerManagement;
+
+    @FXML
+    private Button btnItemManagement;
+
+    @FXML
+    private Button btnOrderDetailsManagement;
+
+    @FXML
+    private Button btnOrderManagement;
+
+    @FXML
+    void btnCustomerManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderManagementOnAction(ActionEvent event) {
+
+    }
+
+}
